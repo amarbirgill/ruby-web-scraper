71a902fae32746aaf026b20fd742b26ffdc99b4d
amarbirgill <amarbir_151@hotmail.com>
2015-10-21 12:35:01 -0700

    create search queries for title, url, post id, points, comments, user id, and time

--- parse.rb
+++ parse.rb
@@ -4,7 +4,14 @@ require 'open-uri'
 class Parse
 
   def initialize(url)
-
+    doc = Nokogiri::HTML(File.open(open(url)))
+    @title = doc.search('.title > a:nth-child(2)').map {|link| link.inner_text}
+    @url = doc.search('.title > a:nth-child(2)').map {|link| link['href']}
+    @post_id = doc.search('.subtext > a:nth-child(3)').map {|link| link['href']}
+    @points = doc.search('.score').map { |span| span.inner_text}
+    @comments = doc.search('.comment > font:first-child').map {|font| font.inner_text}
+    @user_id = doc.search('.comhead > a:first-child').map {|link| link.inner_text}
+    @time = doc.search('.comhead > a:nth-child(2)').map {|link| link.inner_text}
   end  
 end
 
